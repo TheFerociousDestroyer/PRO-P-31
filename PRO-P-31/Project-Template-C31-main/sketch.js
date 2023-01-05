f027b58b126a6d426c5e94ac229056d9d8a0a748
TheFerociousDestroyer <90205431+TheFerociousDestroyer@users.noreply.github.com>
2023-01-05 21:06:00 +0530

    Add files via upload

--- PRO-P-31/Project-Template-C31-main/sketch.js
+++ PRO-P-31/Project-Template-C31-main/sketch.js
@@ -6,129 +6,156 @@ const Constraint = Matter.Constraint;
 const Body = Matter.Body;
 const Composites = Matter.Composites;
 const Composite = Matter.Composite;
-
+var jungleImage,carrotImg,rabbitImg,muteImg,cutButtonImg
+var cl,rrope,carrot
 let engine;
 let world;
-var ground, bridge;
-var leftWall, rightWall;
-var jointPoint;
-var jointLink;
-var zombie1, zombie2, zombie3, zombie4, sadzombie;
-var breakButton;
-var backgroundImage;
-
-var stones = [];
-var collided = false;
-function preload() {
-  zombie1 = loadImage("./assets/zombie1.png");
-  zombie2 = loadImage("./assets/zombie2.png");
-
-  zombie3 = loadImage("./assets/zombie3.png");
-  zombie4 = loadImage("./assets/zombie4.png");
-  sadzombie = loadImage("./assets/sad_zombie.png");
-
-  backgroundImage = loadImage("./assets/background.png");
+var button
+var blinkAnim,eatAnim,sadAnim
+var airWave,eatingSound,ropeCutSound,backgroundSound,sadSound
+function preload()
+{
+  //images
+   jungleImage = loadImage("Jungle.png");
+   carrotImg = loadImage("CARROT.png");
+   rabbitImg = loadImage("Rabbit.png");
+   muteImg = loadImage("mute.png");
+   
+   //animations
+   blinkAnim = loadAnimation("blink_1.png","blink_2.png","blink_3.png")
+   eatAnim = loadAnimation("eat_0.png","eat_1.png","eat_2.png","eat_3.png","eat_4.png")
+   sadAnim = loadAnimation("sad_1.png","sad_2.png","sad_3.png")
+   //sounds
+   airWave = loadSound("air.wav")
+   eatingSound = loadSound("eating_sound.mp3")
+   ropeCutSound = loadSound("rope_cut.mp3")
+   backgroundSound = loadSound("sound1.mp3")
+   sadSound = loadSound("sad.wav")
+  //  cutButtonImg = loadImage("cut_button.png");
+  sadAnim.looping = false
+  eatAnim.looping = false
+  blinkAnim.playing = true
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+
+
+
+function setup() 
+{
+  createCanvas(500,650);
   engine = Engine.create();
   world = engine.world;
-  frameRate(80);
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+backgroundSound.play()
+//button
+button = createImg("cut_button.png")
+button.position(210,30)
+button.size(50,50)
+button.mouseClicked(drop)
 
-  ground = new Base(0, height - 10, width * 2, 20);
-  leftWall = new Base(100, height - 300, 200, height / 2 + 100);
-  rightWall = new Base(width - 100, height - 300, 200, height / 2 + 100);
+//airWaveButton
+airButton = createImg("balloon.png")
+airButton.position(10,250)
+airButton.size(150,100)
+airButton.mouseClicked(airBlow)
 
-  bridge = new Bridge(22, { x: 20, y: 70 });
-  jointPoint = new Base(width - 250, height / 2 - 100, 40, 20);
+//MuteButton
 
-  Matter.Composite.add(bridge.body, jointPoint);
-  jointLink = new Link(bridge, jointPoint);
+muteButton = createImg("mute.png")
+muteButton.position(450,0)
+muteButton.size(50,50)
+muteButton.mouseClicked(mute)
 
-  for (var i = 0; i <= 8; i++) {
-    var x = random(width / 2 - 200, width / 2 + 300);
-    var y = random(-100, 100);
-    var stone = new Stone(x, y, 80, 80);
-    stones.push(stone);
-  }
-
-  zombie = createSprite(width / 2, height - 100, 50, 50);
-  zombie.addAnimation("lefttoright", zombie1, zombie2, zombie1);
-  zombie.addAnimation("righttoleft", zombie3, zombie4, zombie3);
-  zombie.addImage("sad", sadzombie);
-
-  zombie.scale = 0.1;
-  zombie.velocityX = 10;
-
-  breakButton = createButton("");
-  breakButton.position(width - 200, height / 2 - 50);
-  breakButton.class("breakbutton");
-  breakButton.mousePressed(handleButtonPress);
+//bodies
+   ground = new Ground(200,690,600,20)
+   rrope = new Rope(5,{x:245,y:30})
+   carrot = Bodies.circle(300,300,10)
+   Matter.Composite.add(rrope.body,carrot)
+   cl = new Link(rrope,carrot)
+blinkAnim.frameDelay = 7
+   //rabbit
+   rabbit = createSprite(450,550,100,100)
+   rabbit.addAnimation("blinking",blinkAnim)
+   rabbit.addAnimation("eating",eatAnim)
+   rabbit.addAnimation("sading",sadAnim)
+   rabbit.changeAnimation("blinking")
+   rabbit.scale = 0.2
 }
 
-function draw() {
-  background(backgroundImage);
+function draw() 
+{
+  background(51);
+  image(jungleImage,0,0,500,700)
+  ground.show()
+  rrope.show()
+  backgroundSound.setVolume(0.1)
+  if(distance(carrot,rabbit) === true)
+  {
+    rabbit.changeAnimation("eating")
+    eatingSound.play()
+  }
+  if (distance(carrot,ground.body) === true)
+  {
+    rabbit.changeAnimation("sading")
+    sadSound.play()
+  }
+  fill("red")
+  if(carrot!= null)
+  {
+    image(carrotImg,carrot.position.x,carrot.position.y,70,70)
+  }
+
   Engine.update(engine);
-
-  bridge.show();
-
-  for (var stone of stones) {
-    stone.show();
-    var pos = stone.body.position;
-    
-    var distance = dist(zombie.position.x, zombie.position.y, pos.x, pos.y);
-    //var distance = dist(zombie.position.x, zombie.position.y);
-    //var distance = dist(pos.x, pos.y);
-    //var distance = dist(zombie, pos);
+   drawSprites();
+  
+}
 
 
-    /*if (distance >= 50) {
-      zombie.velocityX = 0;
-      Matter.Body.setVelocity(stone.body, { x: 10, y: -10 });
-      zombie.changeImage("sad");
-      collided = true;
-    }*/
+function drop()
+{
+  rrope.break()
+  cl.detach()
+  cl = null
+}
 
-    /*if (distance <= 50) {
-      zombie.velocityX = 0;
-      Matter.Body.setVelocity(stone.body, { x: 10, y: -10 });
-      zombie.Image("sad");
-      collided = true;
-    }*/
 
-    if (distance <= 50) {
-      zombie.velocityX = 0;
-      Matter.Body.setVelocity(stone.body, { x: 10, y: -10 });
-      zombie.changeImage("sad");
-      collided = true;
+
+function distance(body,sprite)
+{
+  if(body!= null)
+  {
+    var d = dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y)
+    if(d<=80)
+    {
+      World.remove(engine.world,body)
+      body = null
+      carrot = null
+      return true
+    }
+    else
+    {
+      return false
     }
 
-    /*if (distance <= 50) {
-      zombie.velocityX = 0;
-      Matter.Body.Velocity(stone.body, { x: 10, y: -10 });
-      zombie.changeImage("sad");
-      collided = true;
-    }*/
-
   }
-
-  if (zombie.position.x >= width - 300 && !collided) {
-    zombie.velocityX = -10;
-    zombie.changeAnimation("righttoleft");
-  }
-
-  if (zombie.position.x <= 300 && !collided) {
-    zombie.velocityX = 10;
-    zombie.changeAnimation("lefttoright");
-  }
-
-  drawSprites();
 }
 
-function handleButtonPress() {
-  jointLink.dettach();
-  setTimeout(() => {
-    bridge.break();
-  }, 1500);
+function airBlow()
+{
+Matter.Body.applyForce(carrot,{x:0,y:0},{x:0.01,y:0})
+airWave.play()
+}
+
+function mute()
+{
+if(backgroundSound.isPlaying())
+{
+backgroundSound.stop()
+}
+else 
+{
+backgroundSound.play()
+}
 }
